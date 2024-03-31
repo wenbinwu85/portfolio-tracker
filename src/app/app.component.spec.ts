49506cd2bf103b259e9c89b7d039cfcd5975d7b9
wenbin wu <ahbenebha@gmail.com>
2024-03-31 14:00:47 -0400

    I am hungry

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'portfolio-tracker' title`, () => {
+  it(`should have the 'bens-awesome-investment-tracker' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('portfolio-tracker');
+    expect(app.title).toEqual('bens-awesome-investment-tracker');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, portfolio-tracker');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, bens-awesome-investment-tracker');
   });
 });
