d2869e4e4ac09d9441eede38a25806855b667e46
Ben <ahbenebha@gmail.com>
2024-11-14 14:05:32 -0500

    meh

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -22,8 +22,8 @@ import { StorageService } from './storage.service';
   providedIn: "root",
 })
 export class DataService {
-  // private backendUrl = "http://127.0.0.1:5000";
-  private backendUrl = "https://portfolio-tracker-backend-5ys2.onrender.com";
+  private backendUrl = "http://127.0.0.1:5000";
+  // private backendUrl = "https://portfolio-tracker-backend-5ys2.onrender.com";
   private httpOptions: any = {
     headers: new HttpHeaders()
       .set("content-type", "application/json")
